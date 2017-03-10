0cae7e2fe0c1b69820f9ca7d37323bdd3d6ee076
Naugtur <naugtur@gmail.com>
2017-03-10 21:55:37 +0100

    finalize tools

---
+++ tools/wrap.js
@@ -0,0 +1,29 @@
+const before = `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>BrikWars Zombie</title>
+  <link rel="stylesheet" href="./style.css">
+</head>
+<body>
+<section>
+  <img src="logo.png" alt="Brainz" class="logo">
+  <h1 id="zombie-brikz" class="title">Zombie Brikz</h1>
+  <h4>AKA Quantum Zombies</h4>
+  <p class="punchline">A game of bricks and the dead. <br> As family friendly as any zombie franchise!</p>
+</section>
+<article>
+`;
+
+const after = `
+</article>
+</body>
+</html>
+`;
+
+process.stdout.write(before)
+process.stdin.pipe(process.stdout)
+process.stdin.on('end',_=>process.stdout.write(after))
